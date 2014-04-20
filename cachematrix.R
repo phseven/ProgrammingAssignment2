0eb460d577929d739e0d50842039193b447b130f
phseven <phseven@gmail.com>
2014-04-20 11:31:39 -0400

    Completed makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix : Caches a symmetric matrix and its inverse.
+## Argument: x = a symmetric matrix whose inverse is to be cached.
+## Returns:  A list of functions (get, set, getInverse, setInverse).
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv_mat <- NULL
+        set <- function(y) {
+                mat <<- y
+                inv_mat <<- NULL
+        }
+        get <- function() mat
+        setInverse <- function(inv = matrix()) inv_mat <<- inv
+        getInverse <- function() inv_mat
 
+	set(x)
+
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:	Returns the inverse of a symmetric matrix from cache if cache is not empty.
+##		Otherwise, computes the inverse of a symmetric matrix, saves it in cache and returns
+## 	      	the inverse matrix.
+# Argument: x = an object created with makeCacheMatrix().
+# Returns:  Inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_mat  <- x$getInverse()
+        if(!is.null(inv_mat)) {
+                message("getting cached data")
+                return(inv_mat)
+        }
+        a_mat <- x$get()
+        inv_mat <- solve(a_mat, ...)
+        x$setInverse(inv_mat)
+        inv_mat
 }
